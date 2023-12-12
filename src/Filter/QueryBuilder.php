c92d1081bc6028e754ec0689cd5d7e615a16d9e9
Tom H Anderson <tom.h.anderson@gmail.com>
2023-12-11 19:23:12 -0700

    Use individual funcitons for QueryBuilder filtering

--- src/Filter/QueryBuilder.php
+++ src/Filter/QueryBuilder.php
@@ -6,6 +6,7 @@ namespace ApiSkeletons\Doctrine\ORM\GraphQL\Filter;
 
 use Doctrine\ORM\QueryBuilder as DoctrineQueryBuilder;
 
+use function method_exists;
 use function uniqid;
 
 /**
@@ -30,73 +31,90 @@ class QueryBuilder
             foreach ($filters as $filter => $value) {
                 $filter = Filters::from($filter);
 
-                switch ($filter) {
-                    case Filters::BETWEEN:
-                        $from = 'p' . uniqid();
-                        $to   = 'p' . uniqid();
-                        $queryBuilder->andWhere(
-                            $queryBuilder->expr()->between(
-                                $entityField,
-                                ':' . $from,
-                                ':' . $to,
-                            ),
-                        )
-                            ->setParameter($from, $value['from'])
-                            ->setParameter($to, $value['to']);
-                        break;
-
-                    case Filters::CONTAINS:
-                        $parameter = 'p' . uniqid();
-                        $queryBuilder->andWhere(
-                            $queryBuilder->expr()->like($entityField, ':' . $parameter),
-                        )
-                            ->setParameter($parameter, '%' . $value . '%');
-                        break;
-
-                    case Filters::STARTSWITH:
-                        $parameter = 'p' . uniqid();
-                        $queryBuilder->andWhere(
-                            $queryBuilder->expr()->like($entityField, ':' . $parameter),
-                        )
-                            ->setParameter($parameter, $value . '%');
-                        break;
-
-                    case Filters::ENDSWITH:
-                        $parameter = 'p' . uniqid();
-                        $queryBuilder->andWhere(
-                            $queryBuilder->expr()->like($entityField, ':' . $parameter),
-                        )
-                            ->setParameter($parameter, '%' . $value);
-                        break;
-
-                    case Filters::ISNULL:
-                        if ($value === true) {
-                            $queryBuilder->andWhere(
-                                $queryBuilder->expr()->isNull($entityField),
-                            );
-                        }
-
-                        if ($value === false) {
-                            $queryBuilder->andWhere(
-                                $queryBuilder->expr()->isNotNull($entityField),
-                            );
-                        }
-
-                        break;
-
-                    case Filters::SORT:
-                        $queryBuilder->addOrderBy($entityField, $value);
-                        break;
-
-                    default:
-                        $parameter = 'p' . uniqid();
-                        $queryBuilder->andWhere(
-                            $queryBuilder->expr()->{$filter->value}($entityField, ':' . $parameter),
-                        )
-                            ->setParameter($parameter, $value);
-                        break;
+                if (method_exists($this, $filter->value) === false) {
+                    $this->default($filter, $entityField, $value, $queryBuilder);
+                } else {
+                    $this->{$filter->value}($entityField, $value, $queryBuilder);
                 }
             }
         }
+    }
+
+    /**
+     * For filters that do not have a special method, use this method
+     */
+    protected function default(Filters $filter, string $field, mixed $value, DoctrineQueryBuilder $queryBuilder): void
+    {
+        $parameter = 'p' . uniqid();
+        $queryBuilder
+            ->andWhere(
+                $queryBuilder->expr()->{$filter->value}($field, ':' . $parameter),
+            )
+            ->setParameter($parameter, $value);
+    }
+
+    /** @param array<string, mixed> $value */
+    protected function between(string $field, array $value, DoctrineQueryBuilder $queryBuilder): void
+    {
+        $from = 'p' . uniqid();
+        $to   = 'p' . uniqid();
+        $queryBuilder
+            ->andWhere(
+                $queryBuilder->expr()->between(
+                    $field,
+                    ':' . $from,
+                    ':' . $to,
+                ),
+            )
+            ->setParameter($from, $value['from'])
+            ->setParameter($to, $value['to']);
+    }
+
+    protected function contains(string $field, string $value, DoctrineQueryBuilder $queryBuilder): void
+    {
+        $parameter = 'p' . uniqid();
+        $queryBuilder
+            ->andWhere(
+                $queryBuilder->expr()->like($field, ':' . $parameter),
+            )
+            ->setParameter($parameter, '%' . $value . '%');
+    }
+
+    public function startsWith(string $field, string $value, DoctrineQueryBuilder $queryBuilder): void
+    {
+        $parameter = 'p' . uniqid();
+        $queryBuilder
+            ->andWhere(
+                $queryBuilder->expr()->like($field, ':' . $parameter),
+            )
+            ->setParameter($parameter, $value . '%');
+    }
+
+    public function endsWith(string $field, string $value, DoctrineQueryBuilder $queryBuilder): void
+    {
+        $parameter = 'p' . uniqid();
+        $queryBuilder
+            ->andWhere(
+                $queryBuilder->expr()->like($field, ':' . $parameter),
+            )
+            ->setParameter($parameter, '%' . $value);
+    }
+
+    public function isnull(string $field, bool $value, DoctrineQueryBuilder $queryBuilder): void
+    {
+        if ($value === true) {
+            $queryBuilder->andWhere(
+                $queryBuilder->expr()->isNull($field),
+            );
+        } else {
+            $queryBuilder->andWhere(
+                $queryBuilder->expr()->isNotNull($field),
+            );
+        }
+    }
+
+    protected function sort(string $field, string $direction, DoctrineQueryBuilder $queryBuilder): void
+    {
+        $queryBuilder->addOrderBy($field, $direction);
     }
 }
