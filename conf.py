ee010b0d01e9b0d63fd43d751ac336d487685c24
Tom H Anderson <tom.h.anderson@gmail.com>
2024-02-09 08:24:14 -0700

    Switching to guzzle_sphinx_theme

--- conf.py
+++ conf.py
@@ -1,3 +1,5 @@
+import guzzle_sphinx_theme
+
 project = 'doctrine-orm-graphql'
 author = 'Tom H Anderson <tom.h.anderson@gmail.com>'
 copyright = '2023 API Skeletons <contact@apiskeletons.com>'
@@ -6,5 +8,13 @@ show_sphinx = True
 commit = True
 last_updated = True
 pygments_style = 'sphinx'
-html_theme = 'sizzle'
 html_favicon = 'favicon.ico'
+
+html_theme_path = guzzle_sphinx_theme.html_theme_path()
+html_theme = 'guzzle_sphinx_theme'
+
+extensions.append("guzzle_sphinx_theme")
+
+html_theme_options = {
+    "project_nav_name": "Doctrine ORM GraphQL",
+}
