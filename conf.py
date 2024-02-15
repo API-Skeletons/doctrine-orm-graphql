1a8a8482926168e7e332d78ccb7cec7736232b69
Tom H Anderson <tom.h.anderson@gmail.com>
2024-02-15 11:18:29 -0700

    Near exact copy of Guzzle conf.py

--- conf.py
+++ conf.py
@@ -11,8 +11,8 @@ extensions = []
 templates_path = ['_templates']
 source_suffix = '.rst'
 master_doc = 'index'
-project = u'doctrine-orm-graphql'
-copyright = u'2024 API Skeletons <contact@apiskeletons.com>'
+project = u'Doctrine ORM GraphQL'
+copyright = u'2024 API Skeletons'
 version = '9'
 html_title = "GraphQL Driver for Doctrine ORM Documentation"
 html_short_title = "Doctrine ORM GraphQL"
@@ -21,12 +21,14 @@ html_favicon = 'favicon.ico'
 exclude_patterns = ['_build']
 html_static_path = ['_static']
 
+##### Guzzle sphinx theme
 
 import guzzle_sphinx_theme
 html_translator_class = 'guzzle_sphinx_theme.HTMLTranslator'
 html_theme_path = guzzle_sphinx_theme.html_theme_path()
 html_theme = 'guzzle_sphinx_theme'
 
+# Custom sidebar templates, maps document names to template names.
 html_sidebars = {
     '**': ['logo-text.html', 'globaltoc.html', 'searchbox.html']
 }
@@ -34,7 +36,34 @@ html_sidebars = {
 # Register the theme as an extension to generate a sitemap.xml
 extensions.append("guzzle_sphinx_theme")
 
+# Guzzle theme options (see theme.conf for more information)
 html_theme_options = {
-    "project_nav_name": "Doctrine ORM GraphQL",
-    "base_url": "https://doctrine-orm-graphql.apiskeletons.dev",
+
+    # Set the path to a special layout to include for the homepage
+    # "index_template": "homepage.html",
+
+    # Allow a separate homepage from the master_doc
+    # homepage = index
+
+    # Set the name of the project to appear in the nav menu
+    # "project_nav_name": "Guzzle",
+
+    # Set your Disqus short name to enable comments
+    # "disqus_comments_shortname": "my_disqus_comments_short_name",
+
+    # Set you GA account ID to enable tracking
+    # "google_analytics_account": "my_ga_account",
+
+    # Path to a touch icon
+    # "touch_icon": "",
+
+    # Specify a base_url used to generate sitemap.xml links. If not
+    # specified, then no sitemap will be built.
+    "base_url": "https://doctrine-orm-graphql.apiskeletons.dev"
+
+    # Allow the "Table of Contents" page to be defined separately from "master_doc"
+    # tocpage = Contents
+
+    # Allow the project link to be overriden to a custom URL.
+    # projectlink = http://myproject.url
 }
