4ec0c92648b6da90685e2012ca74d9a9b7f154bf
Tom H Anderson <tom.h.anderson@gmail.com>
2024-03-26 16:37:45 -0600

    Name between filters after the type name to  avoid many generated Between objects and names

--- src/Filter/InputObjectType/Field.php
+++ src/Filter/InputObjectType/Field.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace ApiSkeletons\Doctrine\ORM\GraphQL\Filter\InputObjectType;
 
 use ApiSkeletons\Doctrine\ORM\GraphQL\Filter\Filters;
+use ApiSkeletons\Doctrine\ORM\GraphQL\Type\TypeManager;
 use GraphQL\Type\Definition\InputObjectType;
 use GraphQL\Type\Definition\ListOfType;
 use GraphQL\Type\Definition\ScalarType;
@@ -17,6 +18,7 @@ class Field extends InputObjectType
 {
     /** @param Filters[] $allowedFilters */
     public function __construct(
+        TypeManager $typeManager,
         string $typeName,
         string $fieldName,
         ScalarType|ListOfType $type,
@@ -31,6 +33,25 @@ class Field extends InputObjectType
                 'type'        => $filter->type($type),
                 'description' => $filter->description(),
             ];
+
+            if (! $type instanceof ScalarType) {
+                continue;
+            }
+
+            if (! ($fields[$filter->value]['type'] instanceof Between)) {
+                continue;
+            }
+
+            // Between is a special case filter.
+            // To avoid creating a new Between type for each field
+            // we check if the Between type exists and reuse it.
+            if ($typeManager->has('Between_' . $type->name)) {
+                $fields[$filter->value]['type'] = $typeManager->get('Between_' . $type->name);
+            } else {
+                $betweenType = new Between($type);
+                $typeManager->set('Between_' . $type->name, $betweenType);
+                $fields[$filter->value]['type'] = $betweenType;
+            }
         }
 
         parent::__construct([
